441ed41327134a48c75cf235629e52c13f4b02b8
Matthew Hutter <matthewjhutter@gmail.com>
2022-10-25 14:18:08 -0700

    progress on items route

---
+++ app/api/item_routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from app.models import db, Item
+
+item_routes = Blueprint('items', __name__)
+
+@item_routes.route("")
+def get_items():
+    """Get all items"""
+    return {'items': }
