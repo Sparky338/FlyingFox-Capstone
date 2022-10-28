b18eb95d6869b992d53a24506db4c3c7f62a5a22
Matthew Hutter <matthewjhutter@gmail.com>
2022-10-28 11:15:48 -0700

    item by id progress

--- react-app/src/components/items/itemById.js
+++ react-app/src/components/items/itemById.js
@@ -1,11 +1,12 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import ImageGallery from 'react-image-gallery';
 
 import "./items.css"
 
 const ItemById = () => {
 
-    const {itemId} = useParams();
+    const { itemId } = useParams();
     const user = useSelector(state => state.session.user);
     const itemsObj = useSelector(state => state.items);
     const items = Object.values(itemsObj);
@@ -16,8 +17,25 @@ const ItemById = () => {
     const filteredItem = items.filter(item => item.id === +itemId)
 
     return (
-        <div>
-
+        <div className="outer-item-div">
+            {filteredItem.map((item) => {
+                return (
+                    <div className="item-by-id" key={item.id}>
+                        <div className="item-outer">
+                            <div className="display-carousel">
+                                {/* <ImageGallery
+                                    items=arrayofImages
+                                    showPlayButton={false}
+                                    showFullscreenButton={false}
+                                    showThumbnails={false}
+                                    autoPlay={true}
+                                    slideInterval={5000}
+                                /> */}
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
