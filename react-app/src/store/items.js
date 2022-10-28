29a6f3dd5e37d427a2c9433d25e90a094a11bd94
Matthew Hutter <matthewjhutter@gmail.com>
2022-10-28 10:49:33 -0700

    connected item_routes to __init__ to populate the store correctly

--- react-app/src/store/items.js
+++ react-app/src/store/items.js
@@ -7,14 +7,14 @@ const getItemsAction = (items) => {
     return {
         type: GET_ITEMS,
         items
-    }
-}
+    };
+};
 
 export const clearItemAction = () => {
     return {
         type: CLEAR_ITEMS
-    }
-}
+    };
+};
 
 
 // Thunks
@@ -24,22 +24,21 @@ export const getAllItems = () => async dispatch => {
     if (res.ok) {
         const items = await res.json();
         dispatch(getItemsAction(items.items));
-    }
+    };
 };
 
 const initialState = {}
 
 // Reducer
 export default function itemsReducer(state = initialState, action) {
-    //normalize data example: businessArr.forEach(business => newState[business.id] = business)
     const newState = { ...state }
     switch (action.type) {
         case GET_ITEMS:
-            action.items.forEach(item => newState[item.id] = item)
+            action.items.forEach((item) => (newState[item.id] = item))
             return newState;
         case CLEAR_ITEMS:
             return {}
         default:
             return state;
-    }
-}
+    };
+};
