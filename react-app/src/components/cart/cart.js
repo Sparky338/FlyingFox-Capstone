915e9a8c705bf9eddf8945bf72a72a268cac0a3e
Matthew Hutter <matthewjhutter@gmail.com>
2022-10-29 14:20:00 -0700

    rough styling of cart laid out

--- react-app/src/components/cart/cart.js
+++ react-app/src/components/cart/cart.js
@@ -8,6 +8,15 @@ const Cart = () => {
                         <div className="your-cart">Your Cart</div>
                         <div className="item-value">(# item)</div>
                     </div>
+                    <div className="cart-contents">
+                        {/* ternary checking if the cart is empty ?
+                        <div className="empty-cart">Your cart is empty!
+                        Perhaps we could interest you in some CATEGORIES?</div> :
+                        <div className="full-cart">
+                            <div className="full-cart-header"> DISPLAY ITEM, QTY, TOTAL</div>
+                            <div className="cart-items"> DISPLAY ITEM PICTURE AND NAME, VALUE, TOTAL PRICE. X TO SHOW A REMOVAL OF ITEM </div>
+                        </div>*/}
+                    </div>
                 </div>
             </div>
 
