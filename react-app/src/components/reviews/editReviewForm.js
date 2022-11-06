2c04a232633ff232c95eb3cf92ecbe663e356391
Matthew Hutter <matthewjhutter@gmail.com>
2022-11-05 23:29:16 -0700

    delete review button added

--- react-app/src/components/reviews/editReviewForm.js
+++ react-app/src/components/reviews/editReviewForm.js
@@ -1,9 +1,12 @@
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, useHistory } from "react-router-dom";
+import { deleteReview } from "../../store/reviews";
 import ReviewForm from "./ReviewForm";
 
 const EditReviewForm = () => {
-    const {reviewId} = useParams();
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const { reviewId } = useParams();
     const currentUser = useSelector(state => state.session.user);
     const reviewsState = useSelector(state => state.reviews);
 
@@ -13,9 +16,24 @@ const EditReviewForm = () => {
     const reviewerId = reviewsState[reviewId]?.user_id;
     const review = reviewsState[reviewId];
 
+    const handleDelete = async () => {
+        let res = window.confirm('Are you sure you want to delete this review?');
+
+        if (res) {
+            const awaitedDelete = await dispatch(deleteReview(reviewId))
+            if (awaitedDelete) alert("Your review has been deleted.")
+            history.push("/")
+        }
+
+    }
+
     if (currentUser.id === reviewerId) {
         return (
-            <ReviewForm storedReview={review} formType="Edit Review" />
+            <>
+                <ReviewForm storedReview={review} formType="Edit Review" />
+                <button className="delete-review-button" onClick={handleDelete}>Delete Review</button>
+            </>
+
         );
     } else return null
 }
