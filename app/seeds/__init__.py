1bbb3eb4bf36358f43f1a9f5fe166ff35514e364
Matthew Hutter <matthewjhutter@gmail.com>
2022-11-07 14:36:11 -0800

    adjusted seed commands to use schema in production

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -5,6 +5,8 @@ from .purchases import seed_purchases, undo_purchases
 from .reviews import seed_reviews, undo_reviews
 from .purchases_items import seed_purchases_items, undo_purchases_items
 
+from app.models.db import db, environment, SCHEMA
+
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
@@ -13,6 +15,15 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    if environment == 'production':
+        # Before seeding, truncate all tables prefixed with schema name
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.items RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.purchases RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.purchases_items RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.reviews RESTART IDENTITY CASCADE;")
+        # Add a truncate command here for every table that will be seeded.
+        db.session.commit()
     seed_users()
     seed_items()
     seed_purchases()
