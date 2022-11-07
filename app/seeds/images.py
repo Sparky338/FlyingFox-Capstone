74cf20fdb9a4cdf8295565d36e70ac25355df99c
Matthew Hutter <matthewjhutter@gmail.com>
2022-11-06 19:43:31 -0800

    image seeders created

--- app/seeds/images.py
+++ app/seeds/images.py
@@ -3,81 +3,406 @@ from app.models import db, Image
 def seed_images():
     Dash = Image(
         item_id ="1",
-        image_url=""
+        image_url="https://squirrel.ws/img/suits/gallery/sprint/gallery/Sprint2.jpg"
+    )
+
+    Dash2 = Image(
+        item_id ="1",
+        image_url="https://squirrel.ws/img/suits/gallery/sprint/gallery/Sprint4.jpg"
+    )
+
+    Dash3 = Image(
+        item_id ="1",
+        image_url="https://squirrel.ws/img/suits/gallery/sprint/gallery/Sprint5.jpg"
     )
 
     River = Image(
         item_id ="2",
-        image_url=""
+        image_url="https://squirrel.ws/img/suits/gallery/swift4/gallery/02-Swift4-580x620.jpg"
+    )
+
+    River2 = Image(
+        item_id ="2",
+        image_url="https://squirrel.ws/img/suits/gallery/swift4/gallery/03-Swift4-v2-580x620.jpg"
+    )
+
+    River3 = Image(
+        item_id ="2",
+        image_url="https://squirrel.ws/img/suits/gallery/swift4/gallery/08-Swift4-580x620.jpg"
+    )
+
+    River4 = Image(
+        item_id ="2",
+        image_url="https://squirrel.ws/img/suits/gallery/swift4/gallery/09-Swift4-580x620.jpg"
+    )
+
+    River5 = Image(
+        item_id ="2",
+        image_url="https://squirrel.ws/img/suits/gallery/swift4/gallery/10-Swift4-580x620.jpg"
     )
 
     BTC = Image(
         item_id ="3",
-        image_url=""
+        image_url="https://squirrel.ws/img/suits/gallery/atc3/gallery/3-ATC3-580x620.jpg"
+    )
+
+    BTC2 = Image(
+        item_id ="3",
+        image_url="https://squirrel.ws/img/suits/gallery/atc3/gallery/6-ATC3-580x620.jpg"
+    )
+
+    BTC3 = Image(
+        item_id ="3",
+        image_url="https://squirrel.ws/img/suits/gallery/atc3/gallery/7-ATC3-580x620.jpg"
+    )
+
+    BTC4 = Image(
+        item_id ="3",
+        image_url="https://squirrel.ws/img/suits/gallery/atc3/gallery/8-ATC3-580x620.jpg"
+    )
+
+    BTC5 = Image(
+        item_id ="3",
+        image_url="https://squirrel.ws/img/suits/gallery/atc3/gallery/11-ATC3-580x620.jpg"
+    )
+
+    BTC6 = Image(
+        item_id ="3",
+        image_url="https://squirrel.ws/img/suits/gallery/atc3/gallery/12-ATC3-580x620.jpg"
     )
 
     Prince = Image(
         item_id ="4",
-        image_url=""
+        image_url="https://squirrel.ws/img/suits/gallery/low-ki/gallery/5-LOWKI-580x620.jpg"
+    )
+
+    Prince2 = Image(
+        item_id ="4",
+        image_url="https://squirrel.ws/img/suits/gallery/low-ki/gallery/3-LOWKI-580x620.jpg"
+    )
+
+    Prince3 = Image(
+        item_id ="4",
+        image_url="https://squirrel.ws/img/suits/gallery/low-ki/gallery/7-LOWKI-580x620.jpg"
+    )
+
+    Prince4 = Image(
+        item_id ="4",
+        image_url="https://squirrel.ws/img/suits/gallery/low-ki/gallery/9-LOWKI-580x620.jpg"
     )
 
     Sideshow = Image(
         item_id ="5",
-        image_url=""
+        image_url="https://squirrel.ws/img/suits/gallery/freak4/gallery/4-Freak4-580x620.jpg"
+    )
+
+    Sideshow2 = Image(
+        item_id ="5",
+        image_url="https://squirrel.ws/img/suits/gallery/freak4/gallery/5-Freak4-580x620.jpg"
+    )
+
+    Sideshow3 = Image(
+        item_id ="5",
+        image_url="https://squirrel.ws/img/suits/gallery/freak4/gallery/7-Freak4-580x620.jpg"
+    )
+
+    Sideshow4 = Image(
+        item_id ="5",
+        image_url="https://squirrel.ws/img/suits/gallery/freak4/gallery/9-Freak4-580x620.jpg"
+    )
+
+    Sideshow5 = Image(
+        item_id ="5",
+        image_url="https://squirrel.ws/img/suits/gallery/freak4/gallery/11-Freak4-580x620.jpg"
     )
 
     Beast = Image(
         item_id ="6",
-        image_url=""
+        image_url="https://squirrel.ws/img/suits/gallery/creature/gallery/2-CREATURE-580x620.jpg"
+    )
+
+    Beast2 = Image(
+        item_id ="6",
+        image_url="https://squirrel.ws/img/suits/gallery/creature/gallery/4-CREATURE-580x620.jpg"
+    )
+
+    Beast3 = Image(
+        item_id ="6",
+        image_url="https://squirrel.ws/img/suits/gallery/creature/gallery/5-CREATURE-580x620.jpg"
+    )
+
+    Beast4 = Image(
+        item_id ="6",
+        image_url="https://squirrel.ws/img/suits/gallery/creature/gallery/9-CREATURE-580x620.jpg"
+    )
+
+    Beast5 = Image(
+        item_id ="6",
+        image_url="https://squirrel.ws/img/suits/gallery/creature/gallery/11-CREATURE-580x620.jpg"
+    )
+
+    Beast6 = Image(
+        item_id ="6",
+        image_url="https://squirrel.ws/img/suits/gallery/creature/gallery/16-CREATURE-580x620.jpg"
+    )
+
+    Beast7 = Image(
+        item_id ="6",
+        image_url="https://squirrel.ws/img/suits/gallery/creature/gallery/19-CREATURE-580x620.jpg"
+    )
+
+    Beast8 = Image(
+        item_id ="6",
+        image_url="https://squirrel.ws/img/suits/gallery/creature/gallery/21-CREATURE-580x620.jpg"
+    )
+
+    Beast9 = Image(
+        item_id ="6",
+        image_url="https://squirrel.ws/img/suits/gallery/creature/gallery/22-CREATURE-580x620.jpg"
     )
 
     Racer = Image(
         item_id ="7",
-        image_url=""
+        image_url="https://squirrel.ws/img/suits/gallery/c-race/gallery/7-C-Race-580x620.jpg"
+    )
+
+    Racer2 = Image(
+        item_id ="7",
+        image_url="https://squirrel.ws/img/suits/gallery/c-race/gallery/8-C-Race-580x620.jpg"
+    )
+
+    Racer3 = Image(
+        item_id ="7",
+        image_url="https://squirrel.ws/img/suits/gallery/c-race/gallery/9-C-Race-580x620.jpg"
     )
 
     Daiko = Image(
         item_id ="8",
-        image_url=""
+        image_url="https://squirrel.ws/img/suits/gallery/sumo3/gallery/02-Sumo3-580x620.jpg"
+    )
+
+    Daiko2 = Image(
+        item_id ="8",
+        image_url="https://squirrel.ws/img/suits/gallery/sumo3/gallery/03-Sumo3-580x620.jpg"
+    )
+
+    Daiko3 = Image(
+        item_id ="8",
+        image_url="https://squirrel.ws/img/suits/gallery/sumo3/gallery/04-Sumo3-580x620.jpg"
+    )
+
+
+    Daiko4 = Image(
+        item_id ="8",
+        image_url="https://squirrel.ws/img/suits/gallery/sumo3/gallery/05-Sumo3-580x620.jpg"
+    )
+
+    Daiko5 = Image(
+        item_id ="8",
+        image_url="https://squirrel.ws/img/suits/gallery/sumo3/gallery/06-Sumo3-580x620.jpg"
+    )
+
+    Daiko6 = Image(
+        item_id ="8",
+        image_url="https://squirrel.ws/img/suits/gallery/sumo3/gallery/08-Sumo3-580x620.jpg"
+    )
+
+    Daiko7 = Image(
+        item_id ="8",
+        image_url="https://squirrel.ws/img/suits/gallery/sumo3/gallery/09-Sumo3-580x620.jpg"
+    )
+
+    Daiko8 = Image(
+        item_id ="8",
+        image_url="https://squirrel.ws/img/suits/gallery/sumo3/gallery/10-Sumo3-580x620.jpg"
     )
 
     Mutant = Image(
         item_id ="9",
-        image_url=""
+        image_url="https://squirrel.ws/img/suits/gallery/mutation2/gallery/04-Mutation2-640x480.jpg"
+    )
+
+    Mutant2 = Image(
+        item_id ="9",
+        image_url="https://squirrel.ws/img/suits/gallery/mutation2/gallery/05-Mutation2-640x480.jpg"
+    )
+
+    Mutant3 = Image(
+        item_id ="9",
+        image_url="https://squirrel.ws/img/suits/gallery/mutation2/gallery/06-Mutation2-640x480.jpg"
+    )
+
+    Mutant4 = Image(
+        item_id ="9",
+        image_url="https://squirrel.ws/img/suits/gallery/mutation2/gallery/07-Mutation2-640x480.jpg"
+    )
+
+    Mutant5 = Image(
+        item_id ="9",
+        image_url="https://squirrel.ws/img/suits/gallery/mutation2/gallery/08-Mutation2-640x480.jpg"
+    )
+
+    Mutant6 = Image(
+        item_id ="9",
+        image_url="https://squirrel.ws/img/suits/gallery/mutation2/gallery/09-Mutation2-640x480.jpg"
     )
 
     SFG = Image(
         item_id ="10",
-        image_url=""
+        image_url="https://squirrel.ws/img/suits/gallery/etmd/gallery/2-ETMD-580x620.jpg"
+    )
+
+    SFG2 = Image(
+        item_id ="10",
+        image_url="https://squirrel.ws/img/suits/gallery/etmd/gallery/3-ETMD-580x620.jpg"
+    )
+
+    SFG3 = Image(
+        item_id ="10",
+        image_url="https://squirrel.ws/img/suits/gallery/etmd/gallery/4-ETMD-580x620.jpg"
+    )
+
+    SFG4 = Image(
+        item_id ="10",
+        image_url="https://squirrel.ws/img/suits/gallery/etmd/gallery/5-ETMD-580x620.jpg"
+    )
+
+    SFG5 = Image(
+        item_id ="10",
+        image_url="https://squirrel.ws/img/suits/gallery/etmd/gallery/8-ETMD-580x620.jpg"
+    )
+
+    SFG6 = Image(
+        item_id ="10",
+        image_url="https://squirrel.ws/img/suits/gallery/etmd/gallery/9-ETMD-580x620.jpg"
     )
 
     Haymaker = Image(
         item_id ="11",
-        image_url=""
+        image_url="https://squirrel.ws/img/suits/gallery/hayduke2/gallery/2-HAYDUKE2-580x620.jpg"
+    )
+
+    Haymaker2 = Image(
+        item_id ="11",
+        image_url="https://squirrel.ws/img/suits/gallery/hayduke2/gallery/3-HAYDUKE2-580x620.jpg"
+    )
+
+    Haymaker3 = Image(
+        item_id ="11",
+        image_url="https://squirrel.ws/img/suits/gallery/hayduke2/gallery/4-HAYDUKE2-580x620.jpg"
+    )
+
+    Haymaker4 = Image(
+        item_id ="11",
+        image_url="https://squirrel.ws/img/suits/gallery/hayduke2/gallery/6-HAYDUKE2-580x620.jpg"
+    )
+
+    Haymaker5 = Image(
+        item_id ="11",
+        image_url="https://squirrel.ws/img/suits/gallery/hayduke2/gallery/8-HAYDUKE2-580x620.jpg"
     )
 
     Epicenter = Image(
         item_id ="12",
-        image_url=""
+        image_url="https://squirrel.ws/img/suits/gallery/epicene-pro/gallery/02-1-EpicenePro-580x620.jpg"
+    )
+
+    Epicenter2 = Image(
+        item_id ="12",
+        image_url="https://squirrel.ws/img/suits/gallery/epicene-pro/gallery/11-EpicenePro-580x620.jpg"
+    )
+
+    Epicenter3 = Image(
+        item_id ="12",
+        image_url="https://squirrel.ws/img/suits/gallery/epicene-pro/gallery/04-EpicenePro-580x620.jpg"
+    )
+
+    Epicenter4 = Image(
+        item_id ="12",
+        image_url="https://squirrel.ws/img/suits/gallery/epicene-pro/gallery/05-EpicenePro-580x620.jpg"
+    )
+
+    Epicenter5 = Image(
+        item_id ="12",
+        image_url="https://squirrel.ws/img/suits/gallery/epicene-pro/gallery/08-EpicenePro-580x620.jpg"
+    )
+
+    Epicenter6 = Image(
+        item_id ="12",
+        image_url="https://squirrel.ws/img/suits/gallery/epicene-pro/gallery/12-EpicenePro-580x620.jpg"
     )
 
     Dash_review = Image(
         review_id ="1",
-        image_url=""
+        image_url="https://www.rockskymarket.com/9498-thickbox_default/squirrel-sprint-wingsuit-jumpsuits.jpg"
     )
 
     db.session.add(Dash)
+    db.session.add(Dash2)
+    db.session.add(Dash3)
     db.session.add(River)
+    db.session.add(River2)
+    db.session.add(River3)
+    db.session.add(River4)
+    db.session.add(River5)
     db.session.add(BTC)
+    db.session.add(BTC2)
+    db.session.add(BTC3)
+    db.session.add(BTC4)
+    db.session.add(BTC5)
+    db.session.add(BTC6)
     db.session.add(Prince)
+    db.session.add(Prince2)
+    db.session.add(Prince3)
+    db.session.add(Prince4)
     db.session.add(Sideshow)
+    db.session.add(Sideshow2)
+    db.session.add(Sideshow3)
+    db.session.add(Sideshow4)
+    db.session.add(Sideshow5)
     db.session.add(Beast)
+    db.session.add(Beast2)
+    db.session.add(Beast3)
+    db.session.add(Beast4)
+    db.session.add(Beast5)
+    db.session.add(Beast6)
+    db.session.add(Beast7)
+    db.session.add(Beast8)
+    db.session.add(Beast9)
     db.session.add(Racer)
+    db.session.add(Racer2)
+    db.session.add(Racer3)
     db.session.add(Daiko)
+    db.session.add(Daiko2)
+    db.session.add(Daiko3)
+    db.session.add(Daiko4)
+    db.session.add(Daiko5)
+    db.session.add(Daiko6)
+    db.session.add(Daiko7)
+    db.session.add(Daiko8)
     db.session.add(Mutant)
+    db.session.add(Mutant2)
+    db.session.add(Mutant3)
+    db.session.add(Mutant4)
+    db.session.add(Mutant5)
+    db.session.add(Mutant6)
     db.session.add(SFG)
+    db.session.add(SFG2)
+    db.session.add(SFG3)
+    db.session.add(SFG4)
+    db.session.add(SFG5)
+    db.session.add(SFG6)
     db.session.add(Haymaker)
+    db.session.add(Haymaker2)
+    db.session.add(Haymaker3)
+    db.session.add(Haymaker4)
+    db.session.add(Haymaker5)
     db.session.add(Epicenter)
+    db.session.add(Epicenter2)
+    db.session.add(Epicenter3)
+    db.session.add(Epicenter4)
+    db.session.add(Epicenter5)
+    db.session.add(Epicenter6)
     db.session.add(Dash_review)
 
     db.session.commit()
