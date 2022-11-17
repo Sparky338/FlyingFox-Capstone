29c497a29b0d797a430fd224e520c661406a705d
Matthew Hutter <matthewjhutter@gmail.com>
2022-11-17 10:47:52 -0800

    created a route and action creator for getting an order's purchases_items, modified the dispatch in orderById to use the new path

--- app/api/purchases_items_routes.py
+++ app/api/purchases_items_routes.py
@@ -11,3 +11,11 @@ def get_user_purchases_items():
     """Get all purchases_items"""
     purchases_items = Purchases_Items.query.all()
     return {'purchases_items': [pi.to_dict() for pi in purchases_items]}
+
+
+@purchases_items_routes.route("/<int:id>")
+@login_required
+def get_order_purchases_items(id):
+    """Get single order's purchases_items"""
+    purchases_items = Purchases_Items.query.filter_by(purchase_id=id).all()
+    return {'purchases_items': [pi.to_dict() for pi in purchases_items]}
--- react-app/src/components/purchases/orderById.js
+++ react-app/src/components/purchases/orderById.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useParams } from "react-router-dom";
-import { getAllPurchasesItems } from "../../store/purchasesItems";
+import { getOrderPurchasesItems } from "../../store/purchasesItems";
 
 import "./orders.css"
 
@@ -15,7 +15,7 @@ const OrderById = () => {
     const purchasesItems = useSelector(state => state.purchasesItems);
 
     useEffect(() => {
-        dispatch(getAllPurchasesItems())
+        dispatch(getOrderPurchasesItems(orderId))
     }, [dispatch])
 
     const formatting_options = {
