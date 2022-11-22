3ca541f9fcb4a6f7828c10f7280c2440b292dd86
Matthew Hutter <matthewjhutter@gmail.com>
2022-11-22 13:32:22 -0800

    updated item by id to reflect changes in location of review image

--- react-app/src/components/items/itemById.js
+++ react-app/src/components/items/itemById.js
@@ -102,9 +102,9 @@ const ItemById = () => {
                                         <div className="review-container" key={i}>
                                             <div className="review">{review.review}</div>
                                             <div className="picture-and-name">
-                                                {!review.images[0] ?
+                                                {!review.image_url ?
                                                     "" :
-                                                    <img className="picture" src={`${review.images[0].image_url}`} />
+                                                    <img className="picture" src={`${review.image_url}`} />
                                                 }
                                                 <div className="first-last-name"> -{review.first_name} {review.last_name} </div>
                                             </div>
