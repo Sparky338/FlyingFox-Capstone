94f103994118c31a09215c23ec50527b8112bffb
Matthew Hutter <matthewjhutter@gmail.com>
2022-11-23 21:28:36 -0800

    developer page filled out and styled

--- react-app/src/components/developer/developer.js
+++ react-app/src/components/developer/developer.js
@@ -1,11 +1,20 @@
+import "./developer.css"
+
 const Developer = () => {
 
     return (
         <div className="developer-outer">
-            <div className="flying-fox-staff">Flying Fox Developer</div>
+            <div className="flying-fox-developer">Flying Fox Developer</div>
             <div className="photo-info-container">
                 <div className="photo-container">
                     <img src="" alt="Image of Developer, Matt Hutter" />
+                </div>
+                <div className="dev-name-links">
+                    <div className="dev-name">Matt Hutter</div>
+                    <div className="dev-links">
+                        <a className="dev-link" href="https://github.com/Sparky338"> GitHub </a>
+                        <a className="dev-link" href="https://www.linkedin.com/in/matthew-hutter-2a6620173"> LinkedIn </a>
+                    </div>
                 </div>
             </div>
         </div>
