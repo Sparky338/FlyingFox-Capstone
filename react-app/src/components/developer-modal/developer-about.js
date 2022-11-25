b6cc6b5f5407328e1e0a26f9e2ae9f6227190ee3
Matthew Hutter <matthewjhutter@gmail.com>
2022-11-24 20:23:10 -0800

    developer about minor change

--- react-app/src/components/developer-modal/developer-about.js
+++ react-app/src/components/developer-modal/developer-about.js
@@ -12,8 +12,8 @@ const DeveloperAbout = () => {
                 <div className="modal-dev-role">Flying Fox Developer</div>
                 <div className="modal-dev-about">
                     Matt is responsible for coding the Flying Fox page you're currently on.
-                    Matt's coding began in the early 2000s during middle school and
-                    high school for projects using HTML. Since then, he has upgraded his skills
+                    Matt began coding in the early 2000s for middle school and
+                    high school projects using HTML. Since then, he has upgraded his skills
                     to code in Javascript, Python, and React/Redux while attending AppAcademy.
                     Matt learned to skydive in 2021 and has loved every second of it.
                     He plans on piloting a wingsuit as soon as he gets the minimum required
