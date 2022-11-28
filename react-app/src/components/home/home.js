ff68ac2af6e48217413d75bc6112c62170813ae5
Matthew Hutter <matthewjhutter@gmail.com>
2022-11-28 08:58:32 -0800

    added ads layout and CSS, text decoration not removing

--- react-app/src/components/home/home.js
+++ react-app/src/components/home/home.js
@@ -1,8 +1,11 @@
 import "./home.css"
 import ImageGallery from 'react-image-gallery';
 import homepageBannerImages from "../../images/homepageBannerImages";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
+    const items = useSelector(state => state.items)
 
     const homeImagesCarousel = () => {
         let images = homepageBannerImages.map(image => {
@@ -38,8 +41,15 @@ const HomePage = () => {
                             <div className="news-articles"></div>
                         </div>
                         <div className="ads-container-right">
-                            <div className="ads-heading">Ads go here</div>
-                            <div className="ads-for-items"></div>
+                            <div className="ads-heading">Featured products</div>
+                            <div className="ads-for-items">
+                                <div className="ad-container">
+                                    <Link to="/items/3">
+                                        <img src={items[3]?.images[3].image_url} className="ad-image" />
+                                        <div className="ad-item-name">{items[3]?.item_name}</div>
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
