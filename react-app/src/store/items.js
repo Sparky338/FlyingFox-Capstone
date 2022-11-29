a398c4055e81126b382c37e4795123ce1b487bb2
Matthew Hutter <matthewjhutter@gmail.com>
2022-11-29 15:37:26 -0800

    removed unused console logs

--- react-app/src/store/items.js
+++ react-app/src/store/items.js
@@ -35,13 +35,8 @@ export default function itemsReducer(state = initialState, action) {
     switch (action.type) {
         case GET_ITEMS:
             action.items.forEach((item) =>
-                (item.images.sort((a,b) => a.id - b.id),
+                (item.images.sort((a, b) => a.id - b.id),
                 newState[item.id] = item))
-                // item.images.sort((a,b) => a.id - b.id)
-                // console.log(item.images)
-
-            // const sortedState = [...newState].sort((a,b) => a.id.images.id - b.id.images.id)
-            // console.log("SORTED STATE", sortedState)
             return newState;
         case CLEAR_ITEMS:
             return {}
