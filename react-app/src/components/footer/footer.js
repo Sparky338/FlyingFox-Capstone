bb194cdcf47202089fc0a24996d330b675c1078d
Matthew Hutter <matthewjhutter@gmail.com>
2022-11-29 15:31:32 -0800

    footer content spaced correctly

--- react-app/src/components/footer/footer.js
+++ react-app/src/components/footer/footer.js
@@ -7,20 +7,18 @@ const Footer = () => {
         <footer>
             <div className="footer-container">
                 <div className="Flying-Fox-repo-footer">
-                    <a className="dev-link-footer" target="_blank" href="https://github.com/Sparky338/FlyingFox-Capstone"> Flying Fox Repo </a>
+                    <a className="repo-link-footer" target="_blank" href="https://github.com/Sparky338/FlyingFox-Capstone"> Flying Fox Repo </a>
                 </div>
                 <div className='dev-footer-container'>
                     <div className='Footer-content'>
                         <img src={flyingFoxLogo} alt="Flying Fox Logo" className='logo-footer' />
-                        {/* <div className="Dev-info-footer"> */}
                         <div className="dev-name-footer">
                             Matthew Hutter
                         </div>
                         <div className="dev-links-footer">
-                            <a className="dev-link-footer" target="_blank" href="https://github.com/Sparky338"> GitHub </a>
-                            <a className="dev-link-footer" target="_blank" href="https://www.linkedin.com/in/matthew-hutter-2a6620173"> LinkedIn </a>
+                            <a className="dev-link-footer" target="_blank" href="https://github.com/Sparky338">GitHub</a>
+                            <a className="dev-link-footer" target="_blank" href="https://www.linkedin.com/in/matthew-hutter-2a6620173">LinkedIn</a>
                         </div>
-                        {/* </div> */}
                         <img src={flyingFoxLogo} alt="Flying Fox Logo" className='logo-footer' />
                     </div>
                 </div>
