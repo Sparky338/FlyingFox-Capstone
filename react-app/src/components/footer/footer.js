b75a4f2463a5449a702b3fae184821a47eb277ca
Matthew Hutter <matthewjhutter@gmail.com>
2022-11-29 14:32:58 -0800

    footer stuck to bottom of page

--- react-app/src/components/footer/footer.js
+++ react-app/src/components/footer/footer.js
@@ -9,17 +9,19 @@ const Footer = () => {
                 <div className="Flying-Fox-repo-footer">
                     <a className="dev-link-footer" target="_blank" href="https://github.com/Sparky338/FlyingFox-Capstone"> Flying Fox Repo </a>
                 </div>
-                <img src={flyingFoxLogo} alt="Flying Fox Logo" className='logo-footer' />
-                <div className="Dev-info-footer">
-                    <div className="dev-name-footer">
-                        Matthew Hutter
+                <div className='Footer-content'>
+                    <img src={flyingFoxLogo} alt="Flying Fox Logo" className='logo-footer' />
+                    {/* <div className="Dev-info-footer"> */}
+                        <div className="dev-name-footer">
+                            Matthew Hutter
+                        </div>
                         <div className="dev-links-footer">
                             <a className="dev-link-footer" target="_blank" href="https://github.com/Sparky338"> GitHub </a>
                             <a className="dev-link-footer" target="_blank" href="https://www.linkedin.com/in/matthew-hutter-2a6620173"> LinkedIn </a>
                         </div>
-                    </div>
+                    {/* </div> */}
+                    <img src={flyingFoxLogo} alt="Flying Fox Logo" className='logo-footer' />
                 </div>
-                <img src={flyingFoxLogo} alt="Flying Fox Logo" className='logo-footer' />
             </div>
         </footer>
     )
