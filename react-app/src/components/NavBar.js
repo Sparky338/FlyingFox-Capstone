01c55df6b819051574adda39d72f3232485245f9
Matthew Hutter <matthewjhutter@gmail.com>
2022-12-01 14:21:11 -0800

    adjusted positioning of columns

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -76,25 +76,27 @@ const NavBar = () => {
                       )
                     })}
                   </div>
-                  <div className='tracking-suits-links-container'>
-                    <div className='tracking-suits-category title Navbar-title'>Tracking Suits</div>
-                    {Object.entries(itemsState).filter(item => item[1].category === "Tracking suit").map((item, i) => {
-                      return (
-                        <Link to={`/items/${item[1].id}`} key={i} className="tracking-suits-links navbar-item-links" >
-                          {(item[1].item_name)}
-                        </Link>
-                      )
-                    })}
-                  </div>
-                  <div className='parachutes-links-container'>
-                    <div className='parachutes-category title Navbar-title'>Parachutes</div>
-                    {Object.entries(itemsState).filter(item => item[1].category === "Parachute").map((item, i) => {
-                      return (
-                        <Link to={`/items/${item[1].id}`} key={i} className="parachute-links navbar-item-links" >
-                          {(item[1].item_name)}
-                        </Link>
-                      )
-                    })}
+                  <div className='track-suit-parachute-container'>
+                    <div className='tracking-suits-links-container'>
+                      <div className='tracking-suits-category title Navbar-title'>Tracking Suits</div>
+                      {Object.entries(itemsState).filter(item => item[1].category === "Tracking suit").map((item, i) => {
+                        return (
+                          <Link to={`/items/${item[1].id}`} key={i} className="tracking-suits-links navbar-item-links" >
+                            {(item[1].item_name)}
+                          </Link>
+                        )
+                      })}
+                    </div>
+                    <div className='parachutes-links-container'>
+                      <div className='parachutes-category title Navbar-title'>Parachutes</div>
+                      {Object.entries(itemsState).filter(item => item[1].category === "Parachute").map((item, i) => {
+                        return (
+                          <Link to={`/items/${item[1].id}`} key={i} className="parachute-links navbar-item-links" >
+                            {(item[1].item_name)}
+                          </Link>
+                        )
+                      })}
+                    </div>
                   </div>
                   <Link to='/developer' className='meet-the-dev title Navbar-title' >Flying Fox Dev</Link>
                 </div>
