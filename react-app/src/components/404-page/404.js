1536f63301515efc67126b6acaa6fc4bbda193f3
Matthew Hutter <matthewjhutter@gmail.com>
2022-12-02 10:38:05 -0800

    404 page styled

--- react-app/src/components/404-page/404.js
+++ react-app/src/components/404-page/404.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'
 import flyingFoxLogo from '../../images/Flying_Fox_Logo.png'
+import "./404.css"
 
 const Page404 = () => {
     return (
         <div className="error-page-container">
-            <div className="Flying-fox-logo-error">
+            <div className="Flying-fox-logo-error-container">
                 <img
                     className='Flying-Fox-logo-error'
                     src={flyingFoxLogo}
@@ -16,7 +17,7 @@ const Page404 = () => {
                     404: Shit, sorry! This page is lost in the wilderness.
                 </div>
                 <div className='line-two-error'>
-                    Please go back to our <Link to="/">home page.</Link>
+                    Please go back to our <Link to="/" className='error-page-link'>home page.</Link>
                 </div>
             </div>
         </div>
