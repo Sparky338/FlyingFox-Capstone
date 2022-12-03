9194f56d16779df22803c87c1176802df1f24611
Matthew Hutter <matthewjhutter@gmail.com>
2022-12-02 17:00:31 -0800

    filter, 404 error handler added

--- react-app/src/components/categories/categories.js
+++ react-app/src/components/categories/categories.js
@@ -1,13 +1,19 @@
+import { useEffect } from "react"
 import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import { errorRedirect } from "../utility/error-redirect"
 
 const Categories = () => {
     const {category} = useParams()
-    const items = useSelector(state => state.items)
+    const itemsObj = useSelector(state => state.items)
+    const items = Object.values(itemsObj);
+
+    useEffect(() => {
+        const categoryItems = items.filter(item => item.category === category)
+    })
 
     let error;
-    if (items) error = errorRedirect(items, category)
+    if (itemsObj) error = errorRedirect(itemsObj, category)
     if (error) return error
 
     return (
