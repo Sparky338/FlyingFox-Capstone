dd3fd95af22347b0e0168d8c34ed0428e502d6a7
Matthew Hutter <matthewjhutter@gmail.com>
2022-12-05 21:22:48 -0800

    created get route for all articles and route to get 4 articles, may need changes

--- app/api/article_routes.py
+++ app/api/article_routes.py
@@ -3,9 +3,16 @@ from flask_login import current_user, login_required
 from app.models import db, Article
 from app.forms import CreateArticle, EditArticle
 
-purchase_routes = Blueprint("articles", __name__)
+article_routes = Blueprint("articles", __name__)
 
-@purchase_routes.route("")
+@article_routes.route("")
 def get_articles():
     """Get all articles"""
-    
+    articles = Article.query.all()
+    return {'articles': [a.to_dict() for a in articles]}
+
+@article_routes("")
+def get_4_articles():
+    """Get 4 articles"""
+    articles = Article.query.all().limit(4)
+    return {'articles':[a.to_dict() for a in articles]}
