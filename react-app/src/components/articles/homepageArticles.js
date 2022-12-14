257de0c64ee0780a0b5cfed3a16f17f45bfb58f1
Matthew Hutter <matthewjhutter@gmail.com>
2022-12-14 15:16:51 -0800

    styled news articles on the homepage

--- react-app/src/components/articles/homepageArticles.js
+++ react-app/src/components/articles/homepageArticles.js
@@ -6,15 +6,15 @@ const HomepageArticles = () => {
         <div className="homepage-articles-container">
             <div className="article-date">12/14/2022</div>
             <div className="article-title-container">
-                <Link to="/news" className="article-title">Here's an article title</Link>
+                <Link to="/news/" className="article-title">Here's an article title</Link>
             </div>
             <div className="article-picture-content">
-                <div className="article-picture">
+                <Link to="/news/" className="article-picture-container">
                     <img src={`https://squirrel.ws/img/news_photos/178/2022-us-nationals-556x556.jpg`}
                          alt="article"
                          className="article-picture"
                     />
-                </div>
+                </Link>
                 <div className="article-content">have a bit of content that eventually cuts off</div>
             </div>
             <Link to="/news">Read More <i class="fa-solid fa-angles-right"></i></Link>
