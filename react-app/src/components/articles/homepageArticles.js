275ae28023b6f4fc4c7522619d2d3d68c3e1c141
Matthew Hutter <matthewjhutter@gmail.com>
2022-12-14 17:01:51 -0800

    created the map for the homepage articles, need to limit them to 4

--- react-app/src/components/articles/homepageArticles.js
+++ react-app/src/components/articles/homepageArticles.js
@@ -1,25 +1,34 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const HomepageArticles = () => {
+    const articlesObj = useSelector(state => state.articles);
+    const reversedArticles = Object.entries(articlesObj).sort((a, b) => a.id - b.id);
 
     return (
-        <div className="homepage-articles-container">
-            <div className="article-date">12/14/2022</div>
-            <div className="article-title-container">
-                <Link to="/news/" className="article-title">Here's an article title</Link>
-            </div>
-            <div className="article-picture-content">
-                <Link to="/news/" className="article-picture-container">
-                    <img src={`https://squirrel.ws/img/news_photos/178/2022-us-nationals-556x556.jpg`}
-                        alt="article"
-                        className="article-picture"
-                    />
-                </Link>
-                <div className="article-content">have a bit of content that eventually cuts off</div>
-            </div>
-            <div className="more-articles-container">
-                <Link to="/news" className="more-articles">Read More <i class="fa-solid fa-angles-right"></i></Link>
-            </div>
+        <div className="homepage-articles-outer">
+            {reversedArticles.map((article, i) => {
+                return (
+                    <div className="homepage-articles-container">
+                        <div className="article-date">12/14/2022</div>
+                        <div className="article-title-container">
+                            <Link to="/news/" className="article-title">Here's an article title</Link>
+                        </div>
+                        <div className="article-picture-content">
+                            <Link to="/news/" className="article-picture-container">
+                                <img src={`https://squirrel.ws/img/news_photos/178/2022-us-nationals-556x556.jpg`}
+                                    alt="article"
+                                    className="article-picture"
+                                />
+                            </Link>
+                            <div className="article-content">have a bit of content that eventually cuts off</div>
+                        </div>
+                        <div className="more-articles-container">
+                            <Link to="/news" className="more-articles">Read More <i class="fa-solid fa-angles-right"></i></Link>
+                        </div>
+                    </div>
+                )
+            })};
         </div>
     )
 }
