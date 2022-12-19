58e7671901f5ca142fce387a66a5c596c0ff468e
Matthew Hutter <matthewjhutter@gmail.com>
2022-12-19 08:44:40 -0800

    added tiny header banner, needs styling

--- react-app/src/components/articles/news.js
+++ react-app/src/components/articles/news.js
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 const NewsPage = () => {
 
+    useEffect(() => {
+        document.body.classList.add('bg-white');
+
+        return function cleanup() {
+            document.body.classList.remove('bg-white');
+        }
+    }, [])
+
     return (
-        <div>
-            test
+        <div className="Newspage-outer">
+            <div className="banner-header">
+            <Link to="/">FlyingFox</Link> {'>'} News
+            </div>
+
         </div>
     )
 }
