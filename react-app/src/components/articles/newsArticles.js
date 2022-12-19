ab588da70e1fc7fc0a7805aed4e2f514fd7f4fcd
Matthew Hutter <matthewjhutter@gmail.com>
2022-12-19 14:18:35 -0800

    styled news article cards, WIP

--- react-app/src/components/articles/newsArticles.js
+++ react-app/src/components/articles/newsArticles.js
@@ -11,13 +11,15 @@ const NewsArticles = () => {
             {reversedArticles.map((article, i) => {
                 return (
                     <div className="news-articles-container" key={i}>
-                        <img src={article[1].image_url}
-                            alt="article"
-                            className="news-article-picture"
-                        />
-                        <div className="news-article-date">{article[1].created_at.split(" ")[0]}</div>
-                        <div className="news-article-title">{article[1].article_title}</div>
-                        <div className="news-article-content">{article[1].article}</div>
+                        <Link to={`news/${article[0]}`} className="news-article-link">
+                            <img src={article[1].image_url}
+                                alt="article"
+                                className="news-article-picture"
+                            />
+                            <div className="news-article-date">{article[1].created_at.split(" ")[0]}</div>
+                            <div className="news-article-title">{article[1].article_title}</div>
+                            <div className="news-article-content">{article[1].article}</div>
+                        </Link>
                     </div>
                 )
             })}
