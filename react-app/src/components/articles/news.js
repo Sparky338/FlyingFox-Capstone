8362c8e53c634efc0aa13984316324be2407a21a
Matthew Hutter <matthewjhutter@gmail.com>
2022-12-19 09:17:19 -0800

    created the main layout for the news page

--- react-app/src/components/articles/news.js
+++ react-app/src/components/articles/news.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import "./articles.css"
+import HomepageArticles from "./homepageArticles";
 
 const NewsPage = () => {
 
@@ -17,10 +18,31 @@ const NewsPage = () => {
         <div className="Newspage-outer">
             <div className="banner-header-container">
                 <div className="banner-header">
-                    <Link to="/" className="banner-header-link">FlyingFox</Link> <span className="spanright">{'>'}</span> News
+                    <Link to="/" className="banner-header-link">FlyingFox</Link>
+                    <span className="spanright">{' > '}</span> News
                 </div>
             </div>
-
+            <div className="news-body-container">
+                <div className="news-header">NEWS</div>
+                <div className="news-search-archive-latest-container">
+                    <div className="news-search-archive-column">
+                        <div className="news-search">
+                            search placeholder
+                        </div>
+                        <div className="news-archive">
+                            NEWS ARCHIVE placeholder
+                        </div>
+                    </div>
+                    <div className="latest-news-column">
+                        <div className="latest-news-header">
+                            LATEST NEWS
+                        </div>
+                        <div className="latest-news-cards">
+                            <HomepageArticles />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
