71f0909562acd34caadbddebd889aea809513cc6
Matthew Hutter <matthewjhutter@gmail.com>
2022-12-19 16:22:19 -0800

    hover overlay displays correctly, needs link-button

--- react-app/src/components/articles/newsArticles.js
+++ react-app/src/components/articles/newsArticles.js
@@ -11,15 +11,16 @@ const NewsArticles = () => {
             {reversedArticles.map((article, i) => {
                 return (
                     <div className="news-articles-container" key={i}>
-                        <Link to={`news/${article[0]}`} className="news-article-link">
-                            <img src={article[1].image_url}
-                                alt="article"
-                                className="news-article-picture"
-                            />
-                            <div className="news-article-date">{article[1].created_at.split(" ")[0]}</div>
-                            <div className="news-article-title">{article[1].article_title}</div>
-                            <div className="news-article-content">{article[1].article}</div>
-                        </Link>
+                            <div className="news-article-link-container">
+                                <img src={article[1].image_url}
+                                    alt="article"
+                                    className="news-article-picture"
+                                />
+                                <div className="news-article-date">{article[1].created_at.split(" ")[0]}</div>
+                                <div className="news-article-title">{article[1].article_title}</div>
+                                <div className="news-article-content">{article[1].article}</div>
+                            </div>
+                        <Link to={`news/${article[0]}`} className="news-article-link"></Link>
                     </div>
                 )
             })}
