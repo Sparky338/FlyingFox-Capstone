e30172fe68d05e37dedb451d42b0dfea008be805
Matthew Hutter <matthewjhutter@gmail.com>
2022-12-22 14:16:35 -0800

    adjustments to seeder data, CSS line for seeder articles to show spaces properly.

--- app/seeds/articles.py
+++ app/seeds/articles.py
@@ -24,6 +24,7 @@ def seed_articles():
     article_sub_three = Article(
         article_title="Canadian Nationals",
         article="""We’d like to send a sincere congratulations to Nichloas, Benoit, and John who shared the podium at the 2021 Canadian Nats, all flying the CR+. Nicholas swept all three events while setting a new Canadian time record. Congrats from all of us at Flying Fox!
+
         Nicholas Yu
         Benoit Sauriol
         John Swallow
@@ -34,8 +35,11 @@ def seed_articles():
     article_sub_two = Article(
         article_title="Chris Geiler & Team USA Win World Championships!",
         article="""The 2021 Wingsuit World Champs event has come to an end with Chris Geiler in first overall flying his Flying Fox CR+. Team USA, all flying the Flying Fox CR+, took gold in the Nations category.
+
         Chris held a commanding lead from the beginning of the competition, finishing more than 10 points ahead of second place for a decisive victory. Alexey Galda (Flying Fox CR+) joined Chris on the podium, and Joe Ridler (Flying Fox CR+) took 5th.
+
         While many of us were relieved that the Worlds event finally happened after a year of delays and months of uncertainty, it’s a bittersweet finale with some of the strongest wingsuit pilots in the world absent due to Covid complications: the Australian team was unable to attend which meant that Flying Fox Team pilots Chris Byrnes and Luke Rogers could not fight for their rightful place in the top 5 overall, and Espen Fadness chose to focus on acrobatics this year – spoiler alert, he almost won! (Espen, Amber and their team won Silver flying their Flying Fox Freaks, just a few points behind gold).
+
         The mission to Siberia was costly and complicated for all who traveled far to be there. All the Flying Fox Team sends a heartfelt and sincere congratulations to the pilots who attended, representing their countries in this highest level event.""",
         image_url="https://squirrel.ws/img/news_photos/172/gallery/1-2021-world-comp-872x530.jpg",
         created_at = "08/23/2021"
@@ -43,6 +47,7 @@ def seed_articles():
     article_sub_one = Article(
         article_title="Fabio, Stefan, Daniel, win German Championships Open",
         article="""Congrats to Fabio Neujahr, the new 2021 German Champion. He was joined on the podium by Stefan Gilbert, and Daniel Ossio, who took 2nd and 3rd overall. All were flying the CR+. Fabio writes, “I won the open class with my CR+ 2020 version. Stefan Gilbert scored second with his CR+ 2021 Version. It was only my 3rd wingsuit competition, and the first one I’ve done in a performance suit after winning the Advanced class at wings over Marl in 2019 and the TonySuit performance cup in 2020.” Awesome work Fabio, and thanks for your support!
+
         Photo attached is of the German National ranking, credit: @t.virus47""",
         image_url="https://squirrel.ws/img/news_photos/173/gallery/1-german-nationals-872x530.jpg",
         created_at = "09/22/2021"
@@ -50,7 +55,9 @@ def seed_articles():
     article_one = Article(
         article_title="Flying Fox Sweeps Podiums at WSPG & Italy Nationals",
         article="""Congrats to Dennis Ohlsen who took first overall at the Wingsuit Performance Games. Luca Sala and Alessandro joined him on the podium taking 2nd and 3rd respectively. Alessandro reports that the vibe was awesome and a fun time was had by all. Pilots from Denmark, Norway, Finland, and Italy were in attendance. Thanks to Skydive Costa d'Argento for hosting!
+
         Also, our congratulations to Luca Sala who is the new Italian Champion. It was a race for second place between Alessandro Urzi and Marco Pistolesi at what was by all accounts another wonderful event at a great location. If you’re into wingsuit performance, don’t miss these events in 2022!
+
         Cheers from all the Flying Fox Team!""",
         image_url="https://squirrel.ws/img/news_photos/174/gallery/1-sqrl-sweeps-podiums-at-wspg-and-italy-nationals-872x530.jpg",
         created_at = "09/27/2021"
@@ -58,6 +65,7 @@ def seed_articles():
     article_two = Article(
         article_title="US Nationals Sweep, New World Records",
         article="""Congrats to our friend Alexey Galda who took gold, and was joined on the podium by Chris Geiler and Joe Ridler. All top five pilots flew Flying Fox. Two new world records were set during the event: the new speed record was set by Chris Geiler, and time was set by Alexey Galda. It’s an honor for us that so many of the world’s top pilots continue to choose Flying Fox. Thank you for your support!
+
         Cheers, from all the Team.""",
         image_url="https://squirrel.ws/img/news_photos/175/gallery/1-2021-us-nationals-sweep-872x530.jpg",
         created_at = "11/01/2021"
@@ -77,8 +85,11 @@ def seed_articles():
     article_five = Article(
         article_title="Flying Fox Pilots Sweep 2022 World Championships",
         article="""Our most heartfelt congratulations to Chris Geiler, new World Champion. He was joined on the podium by Alexey Galda (Silver) and Luke Rogers (Bronze). Joe Ridler and Jason Dodunski rounded out the top 5. Team USA took Gold in the Nations category. All 5 top pilots flew Flying Fox. 15 out of the top 16 pilots flew Flying Fox. And every pilot on the Nations podium flew Flying Fox.
+
         This was the first year that the FAI awarded medals for individual tasks, which meant that Chris, Alexey, and Luke went home with a lot of metal hanging from their necks. Chris took home five FAI medals: Overall Gold, Nations Gold (USA), and Silver medals in Distance, Time, and Speed. Luke and Alexey also picked up Gold medals for Distance and Time, respectively, in addition to their overall podium and nations category medals.
+
         No FAI world event is free of drama, and 2022 was no exception. After Alexey Galda revealed his 3D printed gripper extensions shortly before the event, it set off a scramble amongst a few pilots to replicate the addition to their suits in order to compete in time and distance. The gripper extensions reduced sink rate enough to make a significant improvement to time and distance scores, while reducing the speed score – an obvious tradeoff for the overall title. An informal poll at the event indicated that rigid wing extensions are not favored by competitors and may be banned or restricted in future. We’re excited to see the level of flying increasing so significantly as well as the innovations being explored by pilots in our sport.
+
         A huge thanks to everyone who gave their time and energy to participate in this highest level event, and to all of the pilots who flew Flying Fox. It is a tremendous honor for us to be able to support virtually all of the world’s top pilots.""",
         image_url="https://squirrel.ws/img/news_photos/184/gallery/4-2022-World-Championships-1744x1060.jpg",
         created_at = "10/19/2022"
