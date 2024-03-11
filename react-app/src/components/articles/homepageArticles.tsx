1a054240bc1c4922c9ff1de5866044e2c4e9d09b
Matthew Hutter <matthewjhutter@gmail.com>
2024-03-10 17:16:30 -0700

    converted homepageArticles to TSX

--- react-app/src/components/articles/homepageArticles.tsx
+++ react-app/src/components/articles/homepageArticles.tsx
@@ -1,14 +1,15 @@
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux"; // JS ONLY- does not convert to TS
+import { useAppSelector } from "../utility/hooks"; // Must be used in TS in place of useSelector
 import { Link } from "react-router-dom";
 
 const HomepageArticles = () => { //HOC for articles on homepage
-    const articlesObj = useSelector(state => state.articles);
+    const articlesObj = useAppSelector(state => state.articles);
     const reversedArticles = Object.entries(articlesObj).reverse();
 
 
     return (
         <div className="homepage-articles-outer">
-            {reversedArticles.slice(0, 4).map((article, i) => {
+            {reversedArticles.slice(0, 4).map((article: any, i) => {
                 return (
                     <div className="homepage-articles-container" key={i}>
                         <div className="article-date">{article[1].created_at}</div>
